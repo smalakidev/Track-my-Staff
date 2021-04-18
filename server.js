6283c3b22e9877b4bfadf6cd5a4d2964e5e34c1c
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-04-18 19:27:04 -0400

    Launched the conntection

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
+
+//Here we are conducting the connection to mysql
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1?qK10!+",
+    database: "employee_tracker_db"
+  });
