d77fb58af3588e7a37c3c081bf4a6a8035573440
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-04-19 20:00:13 -0400

    Created function

--- server.js
+++ server.js
@@ -17,3 +17,12 @@ connection.connect(function (err) {
     console.log("Connected as Id" + connection.threadId)
     startPrompt();
 });
+
+//This function initializes the application
+function init() {
+    inquirer.prompt({
+      type: "list",
+      name: "start",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "View All Departments", "View All Roles", "View All Employees By Department", "View All Employees By Manager",
+        "Add Employee", "Remove Employee", "Update Employee Role", "Add Employee Role", "Remove Role", "Add New Department", "Remove Department"]
